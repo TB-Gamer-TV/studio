87ba705c0afde8c15d37d2fd3cf582cd41230446
Thomas <thomas.blackmore100@gmail.com>
2025-08-03 18:53:29 +0000

    this images showcases poor formatting that needs changing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -126,8 +126,8 @@ export default function CharacterSheetPage() {
           <CardHeader>
             <CardTitle>Character Information</CardTitle>
           </CardHeader>
-          <CardContent className="grid grid-cols-1 gap-4 md:grid-cols-4">
-            <div className="space-y-2 md:col-span-2">
+          <CardContent className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-5">
+            <div className="space-y-2 md:col-span-2 lg:col-span-2">
               <Label htmlFor="char-name">Character Name</Label>
               <Input
                 id="char-name"
