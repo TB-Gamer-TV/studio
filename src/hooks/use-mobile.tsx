705f27794727bbbd4c93c7d7782abf826b8795e9
Thomas <thomas.blackmore100@gmail.com>
2025-08-04 08:28:18 +0000

    the buttons o the sidebar take a while to load, can we make this faster

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -2,18 +2,30 @@ import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
+const getIsMobile = () => {
+  if (typeof window === "undefined") {
+    return false
+  }
+  return window.innerWidth < MOBILE_BREAKPOINT
+}
+
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(getIsMobile)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    const onResize = () => {
+      setIsMobile(getIsMobile())
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+
+    window.addEventListener("resize", onResize)
+
+    // Set initial value
+    onResize()
+
+    return () => {
+      window.removeEventListener("resize", onResize)
+    }
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
